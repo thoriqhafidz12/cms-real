95bb631b465ea5d8b055f946aa67ef3fcd3b064d
thoriqhafidz12 <111201911729@mhs.dinus.ac.id>
2026-08-06 09:33:20 +0700

    [UPDATE] INIT

--- resources/views/components/template-bottom.blade.php
+++ resources/views/components/template-bottom.blade.php
@@ -1,13 +1,11 @@
  <!-- Bootstrap core JavaScript-->
- <script src="vendor/jquery/jquery.min.js"></script>
- <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+ <script src="{{ url('/') }}/assets/vendor/jquery/jquery.min.js"></script>
+ <script src="{{ url('/') }}/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
  <!-- Core plugin JavaScript-->
- <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+ <script src="{{ url('/') }}/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
  <!-- Custom scripts for all pages-->
- <script src="js/sb-admin-2.min.js"></script>
-
- </body>
+ <script src="{{ url('/') }}/assets/js/sb-admin-2.min.js"></script>
 
  </html>
