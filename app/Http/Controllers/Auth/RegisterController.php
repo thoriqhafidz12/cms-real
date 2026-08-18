10c763a25883eb70efb0811660860defbf14618b
thoriqhafidz12 <111201911729@mhs.dinus.ac.id>
2026-08-18 15:38:37 +0700

    [UP]

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\BaseController;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
 use Throwable;
